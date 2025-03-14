2d3688b8bbc823aacfd2650bf4bd31ca20c003b5
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2025-03-14 18:37:24 +0300

    module.css

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { heading as headingFontSize } from './assets/04a.module.css'
+import { heading as headingTextColor } from './assets/04b.module.css'
+
+document.querySelector('.heading').classList = `${headingFontSize} ${headingTextColor}`
