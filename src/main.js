1925a56ef3e67f0ab8e03e0f2fab9f011f26c33d
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2025-03-14 21:23:55 +0300

    json module

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-import './assets/05.css'
+import { title } from './assets/06.module.sass';
+import contentStoreData from './assets/contentStore.json'
 
-document.querySelector('.heading').classList = `heading text-3xl font-bold underline`
+(function() {
+	const titleElement = document.querySelector('.title');
+	titleElement.className = title;
+	titleElement.textContent = contentStoreData.title
+
+	const descriptionElement = document.querySelector('.description')
+	descriptionElement.textContent = contentStoreData.description
+})();
